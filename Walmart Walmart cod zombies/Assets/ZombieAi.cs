e3dac93b8fdfc39c93ac5f7e058c3ddcfea4aa19
jimmy <zhengjim@iu.edu>
2026-04-02 17:54:49 -0400

    tick system
    feat:tick system added

--- Walmart Walmart cod zombies/Assets/ZombieAi.cs
+++ Walmart Walmart cod zombies/Assets/ZombieAi.cs
@@ -4,18 +4,30 @@ using UnityEngine.AI;
 public class ZombieAi : MonoBehaviour
 {
     NavMeshAgent agent;
+    Animator animator;
     GameObject player;
-
+    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
         agent.destination = player.transform.position;
+        agent.autoTraverseOffMeshLink = false;
+        
+        TickSystem.frequenttickTime.AddListener(updateLogic);
     }
 
     // Update is called once per frame
-    void Update()
+   
+    void updateLogic(float time) 
     {
+       
         agent.destination = player.transform.position;
+        if (agent.isOnOffMeshLink)
+        {
+           
+        }
     }
+
 }
