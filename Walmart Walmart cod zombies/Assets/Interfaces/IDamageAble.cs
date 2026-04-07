cac1a5c49998c9b5cf070d45f7519d093b9e3c11
jimmy <jimmyzheng8298@gmail.com>
2026-04-07 19:56:23 -0400

    b
    b

--- Walmart Walmart cod zombies/Assets/Interfaces/IDamageAble.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IDamageAble 
-{
-    void takeDamage(float damage);
-
-}
--- Walmart Walmart cod zombies/Assets/woodenBoardScripts/woodenBoardHp.cs
+++ Walmart Walmart cod zombies/Assets/woodenBoardScripts/woodenBoardHp.cs
@@ -1,7 +1,27 @@
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
-public class woodenBoardHp : healthManager
+public class woodenBoardHp : MonoBehaviour, IDamageAble
 {
-    //inheritance go brr   
+    [SerializeField] entityStatSO stats;
+    float health;
+    private void Start()
+    {
+        health = stats.hp;
+    }
+    //param  damageToTake: float to subtract off hp
+    //return the health left after taking damage
+    public float takeDamage(float damageToTake) 
+    {
+        
+        health -= damageToTake;
+        if (health < 0) { health = 0; }
+
+        return health;    
+    }
+    //interface to return the hp upon getting called
+    public float returnHP()
+    {
+        return health;
+    }
 }
